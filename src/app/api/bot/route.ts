8b393c73b0354c092e755c4747336cbc601dea24
Andrey Zhukov <gudvin109@gmail.com>
2024-11-13 21:30:19 +0300

    main

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -7,9 +7,6 @@ const bot = new TelegramBot('7655736393:AAGYAPPjBo1WWKhAXtcUMj0FsTWH35Y7D8g')
 const botUsername = 'naznach_twa_bot'
 const webAppUrl = 'https://naznach.vercel.app'
 
-// Устанавливаем вебхук на этот маршрут
-bot.setWebHook(`https://naznach.vercel.app/api/bot`)
-
 // Основная логика обработки сообщений
 bot.onText(/\/start/, async msg => {
 	const chatId = msg.chat.id
